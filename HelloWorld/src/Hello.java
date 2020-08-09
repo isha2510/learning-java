1ac4d87f948fabade86e61b7a218584b4abdd3e8
isha2510 <ishatripathi80@gmail.com>
2020-08-09 17:00:19 +0530

    Hello world example

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int firstNumber= (10+5)*(100+85);
+        int secondNumber=20;
+        int thirdNumber=firstNumber*3;
+        int Total=firstNumber+secondNumber+thirdNumber;
+        System.out.println(Total);
+    }
+
+}
